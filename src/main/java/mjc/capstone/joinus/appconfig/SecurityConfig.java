e1c32a7cc83d346885b4cd995484c29ac42d8fe9
monikhyun <mih2001103@naver.com>
2025-04-03 09:42:18 +0900

    JWT 삭제 및 내부 로직 수정

--- src/main/java/mjc/capstone/joinus/appconfig/SecurityConfig.java
+++ src/main/java/mjc/capstone/joinus/appconfig/SecurityConfig.java
@@ -2,21 +2,15 @@ package mjc.capstone.joinus.appconfig;
 
 
 import lombok.RequiredArgsConstructor;
-import mjc.capstone.joinus.controller.JwtAuthenticationFilter;
-import mjc.capstone.joinus.provider.JwtTokenProvider;
 import mjc.capstone.joinus.service.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
--- src/main/java/mjc/capstone/joinus/controller/MemberController.java
+++ src/main/java/mjc/capstone/joinus/controller/MemberController.java
@@ -1,14 +1,10 @@
 package mjc.capstone.joinus.controller;
 
 import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import mjc.capstone.joinus.dto.JwtToken;
 import mjc.capstone.joinus.dto.SignInDto;
 import mjc.capstone.joinus.repository.MemberRepository;
-import mjc.capstone.joinus.service.MemberService;
 import mjc.capstone.joinus.service.MemberServiceImpl;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
