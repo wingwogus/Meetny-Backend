0bc2a8492e4671b05406e878e704bf598a68c5a8
monikhyun <mih2001103@naver.com>
2025-04-05 23:51:25 +0900

    feat: 사용자 이미지 정보를 위한 ProfileRequest 추가

---
+++ src/main/java/mjc/capstone/joinus/dto/ProfileRequest.java
@@ -0,0 +1,14 @@
+package mjc.capstone.joinus.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+public class ProfileRequest {
+    private String imageUrl;
+}
