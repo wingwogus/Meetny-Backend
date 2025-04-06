222cc930c67671edb0af32c58032d9dfdca4402f
monikhyun <mih2001103@naver.com>
2025-04-06 19:33:43 +0900

    feat: MyPageService 로직 수정

--- src/main/java/mjc/capstone/joinus/service/MyPageServiceIml.java
+++ src/main/java/mjc/capstone/joinus/service/MyPageServiceIml.java
@@ -6,11 +6,13 @@ import mjc.capstone.joinus.domain.Member;
 import mjc.capstone.joinus.domain.tags.Tag;
 import mjc.capstone.joinus.domain.tags.UserTag;
 import mjc.capstone.joinus.dto.UserDetailDto;
+import mjc.capstone.joinus.dto.UserTagDto;
 import mjc.capstone.joinus.repository.FollowRepository;
 import mjc.capstone.joinus.repository.MemberRepository;
 import mjc.capstone.joinus.repository.TagRepository;
 import mjc.capstone.joinus.repository.UserTagRepository;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,12 +26,28 @@ public class MyPageServiceIml implements MyPageService{
     private final MemberRepository memberRepository;
     private final UserTagRepository userTagRepository;
     private final FollowRepository followRepository;
+    private final TagRepository tagRepository;
 
     @Override
-    public String profileEdit(String url, Member member) {
+    public String profileEdit(String url, String username) {
+        Member member = memberRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("해당 유저를 찾을 수 없습니다."));
         member.setProfileImg(url);
         memberRepository.save(member);
         return url;
+    }
+
+    @Override
+    public Member findMemberByUsername(String username) {
+        return memberRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("해당 유저를 찾을 수 없습니다."));
+    }
+
+
+    @Override
+    public List<Tag> findusertag(Member member) {
+        List<Long> tagIds = userTagRepository.findTagIdsByMember(member);
+        return tagRepository.findAllById(tagIds);
     }
 
     @Override
@@ -53,7 +71,7 @@ public class MyPageServiceIml implements MyPageService{
     }
 
     @Override
-    public UserDetailDto findMember(String username) {
+    public UserDetailDto findMemberDto(String username) {
         return memberRepository.findByUsername(username)
                 .map(member -> new UserDetailDto(
                         member.getNickname(),
@@ -63,6 +81,15 @@ public class MyPageServiceIml implements MyPageService{
                         member.getPhone()
                 ))
                 .orElseThrow(() -> new RuntimeException("사용자를 찾을 수 없습니다."));
+    }
+
+    @Override
+    public void editPassword(Member member, String oldPassword, String newPassword) {
+        if(oldPassword.equals(member.getPassword())) {
+            member.setPassword(newPassword);
+            memberRepository.save(member);
+        }
+
     }
 
     @Override
