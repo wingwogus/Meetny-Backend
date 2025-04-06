a9672f53bc15bc1e8dc2363b4ff55339f99de0eb
이재현 <wingwogus@naver.com>
2025-04-06 15:40:44 +0900

    feat: Post 컨트롤러 구현

---
+++ src/main/java/mjc/capstone/joinus/controller/api/PostController.java
@@ -0,0 +1,47 @@
+package mjc.capstone.joinus.controller.api;
+
+import lombok.RequiredArgsConstructor;
+import mjc.capstone.joinus.domain.Member;
+import mjc.capstone.joinus.dto.CustomUserDetails;
+import mjc.capstone.joinus.dto.PostRequestDto;
+import mjc.capstone.joinus.dto.PostResponseDto;
+import mjc.capstone.joinus.service.PostService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/posts")
+@RequiredArgsConstructor
+public class PostController {
+
+    private final PostService postService;
+    @GetMapping
+    public List<PostResponseDto> getAllPosts() {
+        return postService.getAllPosts();
+    }
+
+//    @GetMapping("/{id}")
+//    public PostResponseDto getPost(@PathVariable Long id) {
+//        return postService.getPost(id);
+//    }
+//
+    @PostMapping
+    public ResponseEntity<String> createPost(@RequestBody PostRequestDto dto, @AuthenticationPrincipal CustomUserDetails userDetails) {
+        postService.createPost(dto, userDetails.getMember());
+        return ResponseEntity.ok("Post created");
+    }
+//
+//    @PutMapping("/{id}")
+//    public void updatePost(@PathVariable Long id, @RequestBody PostRequestDto dto) {
+//        postService.updatePost(id, dto);
+//    }
+//
+//    @DeleteMapping("/{id}")
+//    public void deletePost(@PathVariable Long id) {
+//        postService.deletePost(id);
+//    }
+}
