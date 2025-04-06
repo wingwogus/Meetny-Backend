67a894fd31cc23a20a6e211ea40faf4a7999cbb6
이재현 <wingwogus@naver.com>
2025-04-06 15:38:12 +0900

    fix: ID 값 변경

--- src/main/java/mjc/capstone/joinus/repository/MemberRepository.java
+++ src/main/java/mjc/capstone/joinus/repository/MemberRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface MemberRepository extends JpaRepository<Member, String> {
+public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findUserById(Long id);
     Optional<Member> findByUsername(String username);
