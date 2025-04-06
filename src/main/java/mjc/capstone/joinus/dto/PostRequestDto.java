de7dad30dcdcdffdb4bf56edb421e051000e1da9
이재현 <wingwogus@naver.com>
2025-04-06 15:34:57 +0900

    fix: DTO에 빌더 패턴 추가 및 toEntity 추가

--- src/main/java/mjc/capstone/joinus/dto/PostRequestDto.java
+++ src/main/java/mjc/capstone/joinus/dto/PostRequestDto.java
@@ -1,12 +1,17 @@
 package mjc.capstone.joinus.dto;
 
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import mjc.capstone.joinus.domain.Address;
 import mjc.capstone.joinus.domain.Member;
+import mjc.capstone.joinus.domain.Post;
 import mjc.capstone.joinus.domain.tags.PostTag;
 
 import java.time.LocalDateTime;
 
+@Data
+@NoArgsConstructor
 public class PostRequestDto {
     private String title;
 
@@ -19,4 +24,16 @@ public class PostRequestDto {
     private Address address;
 
     private PostTag postTag;
+
+    public Post toEntity(Member member) {
+        return Post.builder()
+                .title(this.title)
+                .content(this.content)
+                .photo(this.photo)
+                .meetingTime(this.meetingTime)
+                .address(this.address)
+                .postTag(this.postTag)
+                .author(member)
+                .build();
+    }
 }
--- src/main/java/mjc/capstone/joinus/dto/PostResponseDto.java
+++ src/main/java/mjc/capstone/joinus/dto/PostResponseDto.java
@@ -1,16 +1,29 @@
 package mjc.capstone.joinus.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import mjc.capstone.joinus.domain.Address;
+import mjc.capstone.joinus.domain.Post;
 import mjc.capstone.joinus.domain.tags.PostTag;
 
 import java.time.LocalDateTime;
 
+@Data
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
 public class PostResponseDto {
+    private Long id;
+
     private String title;
 
     private String content;
 
     private String photo;
+
+    private String author;
 
     private LocalDateTime meetingTime;
 
@@ -20,5 +33,20 @@ public class PostResponseDto {
 
     private LocalDateTime createdAt;
 
-    private LocalDateTime updatedAt;
+    private LocalDateTime lastModifiedAt;
+
+    public static PostResponseDto from(Post post) {
+        return PostResponseDto.builder()
+                .id(post.getId())
+                .title(post.getTitle())
+                .content(post.getContent())
+                .photo(post.getPhoto())
+                .author(post.getAuthor().getNickname())
+                .meetingTime(post.getMeetingTime())
+                .address(post.getAddress())
+                .postTag(post.getPostTag())
+                .createdAt(post.getCreatedAt())
+                .lastModifiedAt(post.getLastModifiedAt())
+                .build();
+    }
 }
