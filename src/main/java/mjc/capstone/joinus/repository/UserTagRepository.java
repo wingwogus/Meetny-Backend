b8092dff3f6e54b55db88add6f2971255e2f76dc
monikhyun <mih2001103@naver.com>
2025-04-06 19:34:17 +0900

    feat: TagRepository 및 UserTagRepository 메서드 추가

--- src/main/java/mjc/capstone/joinus/repository/UserTagRepository.java
+++ src/main/java/mjc/capstone/joinus/repository/UserTagRepository.java
@@ -4,10 +4,17 @@ import mjc.capstone.joinus.domain.Member;
 import mjc.capstone.joinus.domain.tags.UserTag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserTagRepository extends JpaRepository<UserTag, Long> {
     Optional<UserTag> findByMember(Member member);
+
+    @Query("SELECT t.id FROM UserTag ut JOIN ut.tags t WHERE ut.member = :member")
+    List<Long> findTagIdsByMember(@Param("member") Member member);
 }
+
