3ed070f7ebe1de658b6ade10037484b6f971cf0b
이재현 <wingwogus@naver.com>
2025-04-06 13:59:57 +0900

    feat: PostRepository, PostService 생성

--- src/main/java/mjc/capstone/joinus/repository/PostRepository.java
+++ src/main/java/mjc/capstone/joinus/repository/PostRepository.java
@@ -1,12 +1,15 @@
 package mjc.capstone.joinus.repository;
 
 import mjc.capstone.joinus.domain.Post;
+import mjc.capstone.joinus.domain.tags.Tag;
+import mjc.capstone.joinus.dto.PostResponseDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
-    Optional<Post> findPostById(Long id);
+    List<Post> findPostByTag(Tag tag);
 }
