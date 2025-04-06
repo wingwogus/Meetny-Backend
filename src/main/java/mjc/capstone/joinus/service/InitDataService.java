719265ed7c7b081796856e9d6724ea1d6b0be763
이재현 <wingwogus@naver.com>
2025-04-06 21:48:30 +0900

    feat: InitData 형식 변경

--- src/main/java/mjc/capstone/joinus/service/InitDataService.java
+++ src/main/java/mjc/capstone/joinus/service/InitDataService.java
@@ -5,6 +5,7 @@ import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import mjc.capstone.joinus.domain.Member;
 import mjc.capstone.joinus.domain.Role;
+import mjc.capstone.joinus.dto.SignupRequest;
 import mjc.capstone.joinus.repository.MemberRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -13,18 +14,10 @@ import org.springframework.transaction.annotation.Transactional;
 @Component
 @RequiredArgsConstructor
 public class InitDataService {
-
-    private final MemberRepository memberRepository;
-    private final PasswordEncoder passwordEncoder;
+    private final MemberService memberService;
 
     @PostConstruct
     public void init() {
-        Member member = Member.builder()
-                .username("customuserid")
-                .password(passwordEncoder.encode("1234"))
-                .role(Role.USER)
-                .build();
-
-        memberRepository.save(member);
+        memberService.signup(new SignupRequest("customuserid", "1234"));
     }
 }
