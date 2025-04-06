fe3fc2d205411369ec102eb18108b2aa8def2bf9
이재현 <wingwogus@naver.com>
2025-04-06 13:59:57 +0900

    feat: PostRepository, PostService 생성

--- src/main/java/mjc/capstone/joinus/service/PostService.java
+++ src/main/java/mjc/capstone/joinus/service/PostService.java
@@ -1,7 +1,23 @@
 package mjc.capstone.joinus.service;
 
+import mjc.capstone.joinus.domain.Member;
+import mjc.capstone.joinus.domain.Post;
+import mjc.capstone.joinus.domain.tags.Tag;
+import mjc.capstone.joinus.dto.PostRequestDto;
+import mjc.capstone.joinus.dto.PostResponseDto;
 import org.springframework.stereotype.Service;
+
+import java.nio.file.AccessDeniedException;
+import java.util.List;
 
 @Service
 public interface PostService {
+    void createPost(PostRequestDto post, Member member);
+    void updatePost(PostRequestDto post, Member member);
+    void deletePost(PostRequestDto post, Member member);
+    List<PostResponseDto> findPostByTag(Tag tag);
+    List<PostResponseDto> getAllPosts();
+
+    void validateAuth(Post post, Member member);
+
 }
