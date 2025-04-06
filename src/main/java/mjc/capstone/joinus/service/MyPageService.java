d16f5e99ece10b9381ca5fc397350b98e74c1abf
monikhyun <mih2001103@naver.com>
2025-04-06 19:33:43 +0900

    feat: MyPageService 로직 수정

--- src/main/java/mjc/capstone/joinus/service/MyPageService.java
+++ src/main/java/mjc/capstone/joinus/service/MyPageService.java
@@ -6,20 +6,28 @@ import mjc.capstone.joinus.dto.UserDetailDto;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public interface MyPageService {
     //프로필 이미지 수정
-    String profileEdit(String url, Member member);
+    String profileEdit(String url, String username);
+    // 유저 불러오기
+    Member findMemberByUsername(String username);
+    // 태그 불러오기
+    List<Tag> findusertag(Member member);
     // 태그 추가
     void tagAdd(Tag tag, Member member);
     // 태그 삭제
     void tagRemove(Tag tag, Member member);
     // 유저 정보 가져오기
-    UserDetailDto findMember(String username);
+    UserDetailDto findMemberDto(String username);
     // 팔로워 조회
     void findFollowers(User user);
     // 팔로잉 조회
     void findFollowing(User user);
+    // 비밀번호 수정
+    void editPassword(Member member, String oldPassword, String newPassword);
     // 동행 횟수 조회
 
     // 동행 후기 조회
