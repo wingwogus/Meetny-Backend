30eca0d250910d000b3eb03797dc315230836937
이재현 <wingwogus@naver.com>
2025-04-07 08:45:11 +0900

    fix: PostTag 삭제

--- src/main/java/mjc/capstone/joinus/repository/PostTagRepository.java
+++
@@ -1,9 +0,0 @@
-package mjc.capstone.joinus.repository;
-
-import mjc.capstone.joinus.domain.tags.PostTag;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PostTagRepository extends JpaRepository<PostTag, Long> {
-}
