e60ad57180ce7e54de3ffb05e4b7722a67d1228c
이재현 <wingwogus@naver.com>
2025-04-07 11:37:10 +0900

    fix: signupdto 변경에 따른 메소드 변경

--- src/main/java/mjc/capstone/joinus/service/InitDataService.java
+++ src/main/java/mjc/capstone/joinus/service/InitDataService.java
@@ -3,6 +3,7 @@ package mjc.capstone.joinus.service;
 import jakarta.annotation.PostConstruct;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
+import mjc.capstone.joinus.domain.Gender;
 import mjc.capstone.joinus.domain.Member;
 import mjc.capstone.joinus.domain.Role;
 import mjc.capstone.joinus.dto.SignupRequest;
@@ -18,6 +19,30 @@ public class InitDataService {
 
     @PostConstruct
     public void init() {
-        memberService.signup(new SignupRequest("customuserid", "1234"));
+        memberService.signup(
+                SignupRequest.builder()
+                .username("userid1")
+                .password("1234")
+                .nickname("거인이재현")
+                .gender(Gender.Male)
+                .phone("01073382156")
+                .build());
+        memberService.signup(
+                SignupRequest.builder()
+                .username("userid2")
+                .password("1234")
+                .nickname("소인이재현")
+                .gender(Gender.Male)
+                .phone("01073382156")
+                .build());
+        memberService.signup(
+                SignupRequest.builder()
+                .username("userid3")
+                .password("1234")
+                .nickname("대인이재현")
+                .gender(Gender.Male)
+                .phone("01073382156")
+                .build());
+
     }
 }
