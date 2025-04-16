5f0933dc56218e663d92751ae163042b981769f1
이재현 <wingwogus@naver.com>
2025-04-16 14:52:28 +0900

    dd

--- src/main/java/mjc/capstone/joinus/repository/MemberRepository.java
+++ src/main/java/mjc/capstone/joinus/repository/MemberRepository.java
@@ -9,8 +9,5 @@ import java.util.Optional;
 @Repository
 public interface MemberRepository extends JpaRepository<Member, String> {
 
-    Optional<Member> findUserById(Long id);
     Optional<Member> findByUsername(String username);
-
-    boolean existsByUsername(String username);
 }
