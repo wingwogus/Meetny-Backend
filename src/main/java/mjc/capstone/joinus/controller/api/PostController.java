18f687710eaf1142bb56feac0138519729f5114f
gerrmang <tjdtlghks11@naver.com>
2025-04-16 14:01:47 +0900

    dd

--- src/main/java/mjc/capstone/joinus/controller/api/PostController.java
+++ src/main/java/mjc/capstone/joinus/controller/api/PostController.java
@@ -3,14 +3,18 @@ package mjc.capstone.joinus.controller.api;
 import lombok.RequiredArgsConstructor;
 import mjc.capstone.joinus.domain.Member;
 import mjc.capstone.joinus.domain.Post;
+import mjc.capstone.joinus.domain.tags.Tag;
 import mjc.capstone.joinus.dto.CustomUserDetails;
 import mjc.capstone.joinus.dto.PostRequestDto;
 import mjc.capstone.joinus.dto.PostResponseDto;
+import mjc.capstone.joinus.dto.SearchRequest;
 import mjc.capstone.joinus.service.PostService;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -24,11 +28,6 @@ public class PostController {
         return postService.getAllPosts();
     }
 
-//    @GetMapping("/{id}")
-//    public PostResponseDto getPost(@PathVariable Long id) {
-//        return postService.getPost(id);
-//    }
-//
     @PostMapping
     public ResponseEntity<String> createPost(@RequestBody PostRequestDto dto, @AuthenticationPrincipal CustomUserDetails userDetails) {
         postService.createPost(dto, userDetails.getMember());
@@ -46,4 +45,32 @@ public class PostController {
         postService.deletePost(post, userDetails.getMember());
         return ResponseEntity.ok("Post deleted");
     }
+
+    // 게시글 단건 조회
+    @GetMapping("/{id}")
+    public PostResponseDto getPost(@PathVariable("id") Post post) {
+        return PostResponseDto.from(post);
+    }
+
+    // 태그별 게시글 조회
+    @GetMapping("/tag/{id}")
+    public List<PostResponseDto> getPostsByTag(@PathVariable("id") Tag tag) {
+        return postService.getPostByTag(tag);
+    }
+
+    // 날짜별 게시글 조회
+    @GetMapping("/date")
+    public List<PostResponseDto> getPostsByDate(
+            @RequestBody SearchRequest searchRequest) {
+        return postService.getPostsByDateRange(searchRequest.getFrom(), searchRequest.getTo());
+    }
+
+    // 게시글 키워드 검색
+    @GetMapping("/search")
+    public List<PostResponseDto> searchPosts(@RequestParam("keyword") String keyword) {
+        return postService.searchPosts(keyword);
+    }
+
+
+
 }
--- src/main/java/mjc/capstone/joinus/repository/PostRepository.java
+++ src/main/java/mjc/capstone/joinus/repository/PostRepository.java
@@ -4,10 +4,17 @@ import mjc.capstone.joinus.domain.Post;
 import mjc.capstone.joinus.domain.tags.Tag;
 import mjc.capstone.joinus.dto.PostResponseDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findAllByCreatedAtBetween(LocalDateTime from, LocalDateTime to);
+
+    List<Post> findByTitleOrContentContaining(String keyword);
+
+    List<Post> findByTag(Tag tag);
 }
