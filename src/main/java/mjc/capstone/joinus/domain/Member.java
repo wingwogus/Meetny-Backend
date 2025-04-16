6154950d91fb154b245897456aaffa965fdc7f40
이재현 <wingwogus@naver.com>
2025-04-16 14:58:46 +0900

    post - dev

--- src/main/java/mjc/capstone/joinus/domain/Member.java
+++ src/main/java/mjc/capstone/joinus/domain/Member.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-@Getter
+@Getter @Setter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Member {
 
