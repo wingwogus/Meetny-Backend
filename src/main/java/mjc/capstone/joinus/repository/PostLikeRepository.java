7c82bf71c83ecae7620865cbcccda0d1b415f3e4
이재현 <wingwogus@naver.com>
2025-04-18 13:11:53 +0900

    feat: PostLikeRepository Create

---
+++ src/main/java/mjc/capstone/joinus/repository/PostLikeRepository.java
@@ -0,0 +1,18 @@
+package mjc.capstone.joinus.repository;
+
+import mjc.capstone.joinus.domain.Member;
+import mjc.capstone.joinus.domain.Post;
+import mjc.capstone.joinus.domain.PostLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PostLikeRepository extends JpaRepository<PostLike, Long> {
+    Optional<PostLike> findByMemberAndPost(Member member, Post post);
+
+    int countByPost(Post post);
+
+    boolean existsByMemberAndPost(Member member, Post post);
+
+    void deleteByMemberAndPost(Member member, Post post);
+}
