00a39ac129cdacf7966fbbb2eb2543ebf5bd8f49
Hyeni <jjang010630@naver.com>
2025-05-15 10:21:31 +0900

    feat(PostService): addParticipant 메소드 생성

--- src/main/java/mjc/capstone/joinus/service/inf/PostService.java
+++ src/main/java/mjc/capstone/joinus/service/inf/PostService.java
@@ -35,4 +35,7 @@ public interface PostService {
 
     // 포스트 조회 기능
     PostResponseDto getPostDetail(Post post, Long memberId);
+
+    // 포스트 참가 기능
+    void addParticipant(Long postId, Long memberId);
 }
