8e61ecb7fcfac3d4a37119fbfaccc37ec240b7ba
Hyeni <jjang010630@naver.com>
2025-05-22 10:51:25 +0900

    feat(ReviewPostRepository): findByPostId, findAllByReviewerId 추가

--- src/main/java/mjc/capstone/joinus/repository/ReviewPostRepository.java
+++ src/main/java/mjc/capstone/joinus/repository/ReviewPostRepository.java
@@ -4,6 +4,11 @@ import mjc.capstone.joinus.domain.review.ReviewPost;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface ReviewPostRepository extends JpaRepository<ReviewPost, Long> {
+    Optional<ReviewPost> findByPostId(Long postId);
+    List<ReviewPost> findAllByReviewerId(Long reviewerId);
 }
