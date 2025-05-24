34098614088184f7eaa7ceb9ee8a602087ef7422
이재현 <wingwogus@naver.com>
2025-05-24 10:48:43 +0900

    fix: 채팅 기능 수정

--- src/main/java/mjc/capstone/joinus/service/inf/ChatService.java
+++ src/main/java/mjc/capstone/joinus/service/inf/ChatService.java
@@ -1,21 +1,10 @@
 package mjc.capstone.joinus.service.inf;
 
-import mjc.capstone.joinus.dto.chat.ChatRoomDto;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
+import mjc.capstone.joinus.dto.chat.ChatRequestDto;
+import mjc.capstone.joinus.dto.chat.ChatResponseDto;
 
 public interface ChatService {
-    List<ChatRoomDto> chatRoomList();
+    ChatResponseDto convertMessage(ChatRequestDto dto, String memberId, String roomId);
 
-    @Transactional
-    ChatRoomDto createRoom(String roomName);
-
-    List<String> userList(String roomId);
-
-    @Transactional
-    Long addUser(String roomId, String userName);
-
-    @Transactional
-    void delUser(String roomId, String userName);
+    void saveChat(ChatResponseDto dto);
 }
