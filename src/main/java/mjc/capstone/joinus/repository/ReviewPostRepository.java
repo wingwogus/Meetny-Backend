d21ae99082eca8e0886db652d8602a1585957d43
Hyeni <jjang010630@naver.com>
2025-05-30 00:24:05 +0900

    feat(ReviewPostRepository): countTagsByPostAuthorId 메소드 추가

--- src/main/java/mjc/capstone/joinus/repository/ReviewPostRepository.java
+++ src/main/java/mjc/capstone/joinus/repository/ReviewPostRepository.java
@@ -2,6 +2,8 @@ package mjc.capstone.joinus.repository;
 
 import mjc.capstone.joinus.domain.review.ReviewPost;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,4 +14,13 @@ public interface ReviewPostRepository extends JpaRepository<ReviewPost, Long> {
     Optional<ReviewPost> findByPostId(Long postId);
     List<ReviewPost> findAllByReviewerId(Long reviewerId);
     boolean existsByPostId(Long postId);
+
+    @Query("""
+    SELECT t.reviewTag.tagName, COUNT(t)
+    FROM ReviewPost r
+    JOIN r.mannerTags t
+    WHERE r.post.author.id = :memberId
+    GROUP BY t.reviewTag.tagName
+    """)
+    List<Object[]> countTagsByPostAuthorId(@Param("memberId") Long memberId);
 }
