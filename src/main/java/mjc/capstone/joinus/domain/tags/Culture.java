296c10f273f1e158fa6fe914e0853b460858ed14
이재현 <wingwogus@naver.com>
2025-05-29 11:41:55 +0900

    fix: Exhibition -> Culture

--- src/main/java/mjc/capstone/joinus/domain/tags/Culture.java
+++ src/main/java/mjc/capstone/joinus/domain/tags/Culture.java
@@ -2,15 +2,14 @@ package mjc.capstone.joinus.domain.tags;
 
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 @Entity
 @DiscriminatorValue("Exhibition")
 @RequiredArgsConstructor
-public class Exhibition extends Tag {
+public class Culture extends Tag {
 
-    public Exhibition(String tagName, String color) {
+    public Culture(String tagName, String color) {
         super(tagName, color);
     }
 }
