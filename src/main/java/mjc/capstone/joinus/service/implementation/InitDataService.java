89be38eeef2a1f030286fbcb4ee6f53db16e4adb
Hyeni <jjang010630@naver.com>
2025-05-30 01:27:23 +0900

    merge

--- src/main/java/mjc/capstone/joinus/service/implementation/InitDataService.java
+++ src/main/java/mjc/capstone/joinus/service/implementation/InitDataService.java
@@ -2,11 +2,17 @@ package mjc.capstone.joinus.service.implementation;
 
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+<<<<<<< Updated upstream
 import mjc.capstone.joinus.domain.entity.Address;
 import mjc.capstone.joinus.domain.entity.Gender;
 import mjc.capstone.joinus.domain.entity.Member;
 import mjc.capstone.joinus.domain.entity.Role;
 import mjc.capstone.joinus.domain.entity.*;
+=======
+import mjc.capstone.joinus.domain.entity.*;
+import mjc.capstone.joinus.domain.review.ReviewTag;
+import mjc.capstone.joinus.domain.review.ReviewTagType;
+>>>>>>> Stashed changes
 import mjc.capstone.joinus.domain.tags.*;
 import mjc.capstone.joinus.repository.MemberRepository;
 import mjc.capstone.joinus.repository.PostRepository;
@@ -27,13 +33,17 @@ public class InitDataService {
     private final UserTagRepository userTagRepository;
     private final PasswordEncoder passwordEncoder;
     private final PostRepository postRepository;
+<<<<<<< Updated upstream
     private final MemberServiceImpl memberService;
 
 
+=======
+>>>>>>> Stashed changes
 
     @PostConstruct
     public void init() {
 
+<<<<<<< Updated upstream
         Tag rockTag = tagRepository.save(new Concert("락", "#FF5733"));
         Tag balladeTag = tagRepository.save(new Concert("발라드", "#33C1FF"));
         Tag exhibitionTag = tagRepository.save(new Exhibition("전시회", "#9D33FF"));
@@ -47,6 +57,59 @@ public class InitDataService {
                 fairTag,
                 horribleTag,
                 soccerTag); // 락, 발라드, 전시회, 공포, 야구
+=======
+        Concert jpopTag = new Concert("J-POP", "#FF5733");
+        Concert rockTag = new Concert("락", "#33C1FF");
+        Concert balladeTag = new Concert("발라드", "#75FF33");
+        Concert hiphopTag = new Concert("힙합", "#FF33EC");
+        Concert kpopTag = new Concert("K-POP", "#FFBD33");
+        Concert edmTag = new Concert("EDM", "#8D33FF");
+        Concert popTag = new Concert("POP", "#33FFBD");
+        Concert trotTag = new Concert("트로트", "#FF3333");
+        Concert jazzTag = new Concert("JAZZ", "#33C1FF");
+
+        Culture museumTag = new Culture("박물관", "#75FF33");
+        Culture galleryTag = new Culture("미술관", "#FF33EC");
+        Culture popupTag = new Culture("팝업", "#FFBD33");
+        Culture expoTag = new Culture("박람회", "#8D33FF");
+        Culture contestTag = new Culture("콘테스트", "#33FFBD");
+        Culture exhibitionTag = new Culture("전시회", "#FF5733");
+        Culture musicalTag = new Culture("뮤지컬", "#FF3333");
+        Culture playTag = new Culture("연극", "#33C1FF");
+        Culture comedyShowTag = new Culture("코미디 쇼", "#75FF33");
+
+        Sports soccerTag = new Sports("축구", "#FF33EC");
+        Sports baseballTag = new Sports("야구", "#FFBD33");
+        Sports basketballTag = new Sports("농구", "#8D33FF");
+        Sports volleyballTag = new Sports("배구", "#33FFBD");
+
+        Movie actionTag = new Movie("액션", "#FF3333");
+        Movie romanceTag = new Movie("로맨스", "#33C1FF");
+        Movie comedyTag = new Movie("코미디", "#75FF33");
+        Movie thrillerTag = new Movie("스릴러/호러", "#FF33EC");
+        Movie fantasyTag = new Movie("판타지/SF", "#FFBD33");
+        Movie documentaryTag = new Movie("다큐멘터리", "#8D33FF");
+        Movie animationTag = new Movie("애니메이션", "#33FFBD");
+        Movie musicalMovieTag = new Movie("뮤지컬", "#FF5733");
+        Movie dramaTag = new Movie("드라마", "#FF3333");
+
+        ReviewTag goodManner = new ReviewTag("친절하고 매너가 좋아요", ReviewTagType.POSITIVE);
+        ReviewTag punctual = new ReviewTag("시간 약속을 잘 지켜요", ReviewTagType.POSITIVE);
+        ReviewTag goodCommunication = new ReviewTag("소통이 원활해요", ReviewTagType.POSITIVE);
+        ReviewTag considerate = new ReviewTag("배려심이 깊어요", ReviewTagType.POSITIVE);
+
+        ReviewTag unpunctual = new ReviewTag("시간 약속을 지키지 않아 아쉬웠어요", ReviewTagType.NEGATIVE);
+        ReviewTag badCommunication = new ReviewTag("소통이 원활하지 않았어요", ReviewTagType.NEGATIVE);
+        ReviewTag verbalAbuse = new ReviewTag("폭언 및 욕설을 해요", ReviewTagType.NEGATIVE);
+        ReviewTag inappropriateTalk = new ReviewTag("불편한 주제로 대화를 해요", ReviewTagType.NEGATIVE);
+
+        List<Tag> selectedTags = List.of(
+                jpopTag, rockTag, balladeTag, hiphopTag, kpopTag, edmTag, popTag, trotTag, jazzTag,
+                museumTag, galleryTag, popupTag, expoTag, contestTag, exhibitionTag, musicalTag, playTag, comedyShowTag,
+                soccerTag, baseballTag, basketballTag, volleyballTag,
+                actionTag, romanceTag, comedyTag, thrillerTag, fantasyTag, documentaryTag, animationTag, musicalMovieTag, dramaTag
+        );
+>>>>>>> Stashed changes
 
         Member member = Member.builder()
                 .username("mih2001103")
@@ -128,10 +191,8 @@ public class InitDataService {
 
 
         List<Tag> tag1 = List.of(
-                new Concert("락", "#FF5733"),
-                new Exhibition("전시회", "#9D33FF"));
-
-        tagRepository.saveAll(tag1);
+                rockTag,
+                exhibitionTag);
 
         for (Tag tag : tag1) {
             MemberTag memberTag = new MemberTag();
@@ -141,10 +202,8 @@ public class InitDataService {
         }
 
         List<Tag> tag2 = List.of(
-                new Movie("공포", "#FF3333"),
-                new Sports("야구", "#33FF57"));
-
-        tagRepository.saveAll(tag2);
+                hiphopTag,
+                balladeTag);
 
         for (Tag tag : tag2) {
             MemberTag memberTag = new MemberTag();
@@ -154,10 +213,8 @@ public class InitDataService {
         }
 
         List<Tag> tag3 = List.of(
-                new Concert("랩", "#FF33A8"),
-                new Sports("축구", "#335BFF"));
-
-        tagRepository.saveAll(tag3);
+                musicalTag,
+                soccerTag);
 
         for (Tag tag : tag3) {
             MemberTag memberTag = new MemberTag();
@@ -199,6 +256,42 @@ public class InitDataService {
 
         postRepository.save(post2);
 
+<<<<<<< Updated upstream
+=======
+        Post post1 = Post.builder()
+                .author(member1)
+                .title("토트넘 내한 동행 구인")
+                .content("토트넘 내한 동행 구해용")
+                .tag(soccerTag)
+                .meetingTime(LocalDateTime.now().plusDays(3))
+                .photo("url/dummyImg")
+                .address(
+                        Address.builder()
+                                .city("서울시")
+                                .town("마포구")
+                                .street("월드컵로 240")
+                                .build())
+                .build();
+
+        postRepository.save(post1);
+
+        Post post2 = Post.builder()
+                .author(member2)
+                .title("상상용 내한 동행 구함")
+                .content("여자만 받아요")
+                .tag(rockTag)
+                .meetingTime(LocalDateTime.now().plusDays(10))
+                .photo("url/dummyImg")
+                .address(
+                        Address.builder()
+                                .city("서울시")
+                                .town("마포구")
+                                .street("월드컵로 240")
+                                .build())
+                .build();
+
+        postRepository.save(post2);
+>>>>>>> Stashed changes
     }
 
 }
