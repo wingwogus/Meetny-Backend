a05116b8f21a4fb3f284b262a9fc9beae2b1d465
Hyeni <jjang010630@naver.com>
2025-05-30 00:15:05 +0900

    feat(CredibilityResponseDto): CredibilityResponseDto 추가

---
+++ src/main/java/mjc/capstone/joinus/dto/CredibilityResponseDto.java
@@ -0,0 +1,10 @@
+package mjc.capstone.joinus.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CredibilityResponseDto {
+    private Double credibility;
+}
