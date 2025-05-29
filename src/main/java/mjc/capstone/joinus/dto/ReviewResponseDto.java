d5f12efea403b4e9f345a521b2d39bc7e4e03712
Hyeni <jjang010630@naver.com>
2025-05-30 00:23:21 +0900

    feat(ReviewResponseDto): 포스트 사진, meetingTime 추가

--- src/main/java/mjc/capstone/joinus/dto/ReviewResponseDto.java
+++ src/main/java/mjc/capstone/joinus/dto/ReviewResponseDto.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import mjc.capstone.joinus.domain.review.ReviewPost;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,12 +19,16 @@ public class ReviewResponseDto {
     private String comment;
     private String reviewerNickname;
     private String postTitle;
+    private String photo;
+    private LocalDateTime meetingTime;
     private List<String> mannerTag = new ArrayList<>();
 
     public static ReviewResponseDto from(ReviewPost review){
         return ReviewResponseDto.builder()
                 .reviewerId(review.getReviewer().getId())
                 .comment(review.getComment())
+                .photo(review.getPost().getPhoto())
+                .meetingTime(review.getPost().getMeetingTime())
                 .reviewerNickname(review.getReviewer().getNickname())
                 .postTitle(review.getPost().getTitle())
                 .mannerTag(
