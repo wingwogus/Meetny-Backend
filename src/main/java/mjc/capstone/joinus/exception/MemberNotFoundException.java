22ecb2400a73f3f2d5ee885c432594639393051b
Hyeni <jjang010630@naver.com>
2025-05-30 19:15:24 +0900

    refactor: 중복된 예외 클래스 정리 및 제거

--- src/main/java/mjc/capstone/joinus/exception/MemberNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package mjc.capstone.joinus.exception;
-
-public class MemberNotFoundException extends BusinessException{
-
-    public MemberNotFoundException(){
-        super(ErrorCode.MEMBER_NOT_FOUND, "유효하지 않은 사용자입니다");
-    }
-}
--- src/main/java/mjc/capstone/joinus/exception/PostNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package mjc.capstone.joinus.exception;
-
-public class PostNotFoundException extends BusinessException{
-
-    public PostNotFoundException(){
-        super(ErrorCode.POST_NOT_FOUND, "존재하지 않는 게시물입니다.");
-    }
-}
