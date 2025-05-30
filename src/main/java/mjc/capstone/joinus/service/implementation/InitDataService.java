f472585d402f4b3aaf88486844593ad40fc5b147
Hyeni <jjang010630@naver.com>
2025-05-30 19:51:13 +0900

    테스트케이스 수정

--- src/main/java/mjc/capstone/joinus/service/implementation/InitDataService.java
+++ src/main/java/mjc/capstone/joinus/service/implementation/InitDataService.java
@@ -11,7 +11,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -21,11 +20,56 @@ public class InitDataService {
     private final MemberRepository memberRepository;
     private final UserTagRepository userTagRepository;
     private final PasswordEncoder passwordEncoder;
-    private final ReviewTagRepository reviewTagRepository;
     private final PostRepository postRepository;
+    private final ReviewTagRepository reviewTagRepository;
 
     @PostConstruct
     public void init() {
+
+        Concert jpopTag = new Concert("J-POP", "#FF5733");
+        Concert rockTag = new Concert("락", "#33C1FF");
+        Concert balladeTag = new Concert("발라드", "#75FF33");
+        Concert hiphopTag = new Concert("힙합", "#FF33EC");
+        Concert kpopTag = new Concert("K-POP", "#FFBD33");
+        Concert edmTag = new Concert("EDM", "#8D33FF");
+        Concert popTag = new Concert("POP", "#33FFBD");
+        Concert trotTag = new Concert("트로트", "#FF3333");
+        Concert jazzTag = new Concert("JAZZ", "#33C1FF");
+
+        Culture museumTag = new Culture("박물관", "#75FF33");
+        Culture galleryTag = new Culture("미술관", "#FF33EC");
+        Culture popupTag = new Culture("팝업", "#FFBD33");
+        Culture expoTag = new Culture("박람회", "#8D33FF");
+        Culture contestTag = new Culture("콘테스트", "#33FFBD");
+        Culture exhibitionTag = new Culture("전시회", "#FF5733");
+        Culture musicalTag = new Culture("뮤지컬", "#FF3333");
+        Culture playTag = new Culture("연극", "#33C1FF");
+        Culture comedyShowTag = new Culture("코미디 쇼", "#75FF33");
+
+        Sports soccerTag = new Sports("축구", "#FF33EC");
+        Sports baseballTag = new Sports("야구", "#FFBD33");
+        Sports basketballTag = new Sports("농구", "#8D33FF");
+        Sports volleyballTag = new Sports("배구", "#33FFBD");
+
+        Movie actionTag = new Movie("액션", "#FF3333");
+        Movie romanceTag = new Movie("로맨스", "#33C1FF");
+        Movie comedyTag = new Movie("코미디", "#75FF33");
+        Movie thrillerTag = new Movie("스릴러/호러", "#FF33EC");
+        Movie fantasyTag = new Movie("판타지/SF", "#FFBD33");
+        Movie documentaryTag = new Movie("다큐멘터리", "#8D33FF");
+        Movie animationTag = new Movie("애니메이션", "#33FFBD");
+        Movie musicalMovieTag = new Movie("뮤지컬", "#FF5733");
+        Movie dramaTag = new Movie("드라마", "#FF3333");
+
+        List<Tag> selectedTags = List.of(
+                jpopTag, rockTag, balladeTag, hiphopTag, kpopTag, edmTag, popTag, trotTag, jazzTag,
+                museumTag, galleryTag, popupTag, expoTag, contestTag, exhibitionTag, musicalTag, playTag, comedyShowTag,
+                soccerTag, baseballTag, basketballTag, volleyballTag,
+                actionTag, romanceTag, comedyTag, thrillerTag, fantasyTag, documentaryTag, animationTag, musicalMovieTag, dramaTag
+        );
+
+        tagRepository.saveAll(selectedTags);
+
         ReviewTag goodManner = new ReviewTag("친절하고 매너가 좋아요", ReviewTagType.POSITIVE);
         ReviewTag punctual = new ReviewTag("시간 약속을 잘 지켜요", ReviewTagType.POSITIVE);
         ReviewTag goodCommunication = new ReviewTag("소통이 원활해요", ReviewTagType.POSITIVE);
@@ -57,25 +101,16 @@ public class InitDataService {
                         .town("방이동")
                         .build())
                 .gender(Gender.MALE)
-                .role(Role.USER)
                 .credibility(45.0)
+                .role(Role.USER)
                 .profileImg("https://ui-avatars.com/api/?name=Jae+Hyun&background=random")
                 .password(passwordEncoder.encode("1234"))
                 .build();
 
         memberRepository.save(member);
 
-        List<Tag> selectedTags = List.of(new Concert("락", "#FF5733"),
-                new Concert("발라드", "#33C1FF"),
-                new Culture("전시회", "#9D33FF"),
-                new Culture("박람회", "#33FFBD"),
-                new Movie("공포", "#FF3333"),
-                new Sports("농구", "#FF8C33")); // 락, 발라드, 전시회, 공포, 야구
 
-        tagRepository.saveAll(selectedTags);
-
-        for (
-                Tag tag : selectedTags) {
+        for (Tag tag : selectedTags) {
             MemberTag memberTag = new MemberTag();
             memberTag.setMember(member);
             memberTag.setTags(tag);
@@ -93,8 +128,8 @@ public class InitDataService {
                         .town("방이동")
                         .build())
                 .gender(Gender.MALE)
-                .role(Role.USER)
                 .credibility(45.0)
+                .role(Role.USER)
                 .profileImg("https://ui-avatars.com/api/?name=Jae+Hyun&background=random")
                 .password(passwordEncoder.encode("1234"))
                 .build();
@@ -110,8 +145,8 @@ public class InitDataService {
                         .town("응암동")
                         .build())
                 .gender(Gender.MALE)
-                .role(Role.USER)
                 .credibility(45.0)
+                .role(Role.USER)
                 .profileImg("https://ui-avatars.com/api/?name=Jae+Hyun&background=random")
                 .password(passwordEncoder.encode("1234"))
                 .build();
@@ -127,8 +162,8 @@ public class InitDataService {
                         .town("홍제동")
                         .build())
                 .gender(Gender.MALE)
-                .role(Role.USER)
                 .credibility(45.0)
+                .role(Role.USER)
                 .profileImg("https://ui-avatars.com/api/?name=Jae+Hyun&background=random")
                 .password(passwordEncoder.encode("1234"))
                 .build();
@@ -139,13 +174,10 @@ public class InitDataService {
 
 
         List<Tag> tag1 = List.of(
-                new Concert("락", "#FF5733"),
-                new Culture("전시회", "#9D33FF"));
+                rockTag,
+                exhibitionTag);
 
-        tagRepository.saveAll(tag1);
-
-        for (
-                Tag tag : tag1) {
+        for (Tag tag : tag1) {
             MemberTag memberTag = new MemberTag();
             memberTag.setMember(member1);
             memberTag.setTags(tag);
@@ -153,13 +185,10 @@ public class InitDataService {
         }
 
         List<Tag> tag2 = List.of(
-                new Movie("공포", "#FF3333"),
-                new Sports("야구", "#33FF57"));
+                hiphopTag,
+                balladeTag);
 
-        tagRepository.saveAll(tag2);
-
-        for (
-                Tag tag : tag2) {
+        for (Tag tag : tag2) {
             MemberTag memberTag = new MemberTag();
             memberTag.setMember(member2);
             memberTag.setTags(tag);
@@ -167,30 +196,50 @@ public class InitDataService {
         }
 
         List<Tag> tag3 = List.of(
-                new Concert("랩", "#FF33A8"),
-                new Sports("축구", "#335BFF"));
+                musicalTag,
+                soccerTag);
 
-        tagRepository.saveAll(tag3);
-
-        for (
-                Tag tag : tag3) {
+        for (Tag tag : tag3) {
             MemberTag memberTag = new MemberTag();
             memberTag.setMember(member3);
             memberTag.setTags(tag);
             userTagRepository.save(memberTag);
         }
 
-        Post post = Post.builder()
-                .title("테스트 동행 모집")
-                .content("테스트용으로 만든 게시글입니다. 시간 맞는 분 함께해요.")
-                .photo(null)
-                .meetingTime(LocalDateTime.now().plusDays(2))
-                .address(member.getAddress())
-                .tag(tag1.getFirst())
+        Post post1 = Post.builder()
+                .author(member1)
+                .title("토트넘 내한 동행 구인")
+                .content("토트넘 내한 동행 구해용")
+                .tag(soccerTag)
+                .meetingTime(LocalDateTime.now().plusDays(3))
+                .photo("url/dummyImg")
+                .address(
+                        Address.builder()
+                                .city("서울시")
+                                .town("마포구")
+                                .street("월드컵로 240")
+                                .build())
+                .participant(member2)
                 .build();
-        member2.setPosts(new ArrayList<>());
-        post.setAuthor(member2); // 연관관계 메소드
-        post.setParticipant(member1);
-        postRepository.save(post);
+
+        postRepository.save(post1);
+
+        Post post2 = Post.builder()
+                .author(member2)
+                .title("상상용 내한 동행 구함")
+                .content("여자만 받아요")
+                .tag(rockTag)
+                .meetingTime(LocalDateTime.now().plusDays(10))
+                .photo("url/dummyImg")
+                .address(
+                        Address.builder()
+                                .city("서울시")
+                                .town("마포구")
+                                .street("월드컵로 240")
+                                .build())
+                .participant(member3)
+                .build();
+
+        postRepository.save(post2);
     }
 }
