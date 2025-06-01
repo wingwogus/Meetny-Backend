e74bdbe84819ffc8779c5798103d2ec2f0123592
monikhyun <mih2001103@naver.com>
2025-06-02 02:33:48 +0900

    feat : dev 작업 내용 이전

---
+++ src/main/java/mjc/capstone/joinus/domain/entity/Member.java
@@ -0,0 +1,70 @@
+package mjc.capstone.joinus.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import mjc.capstone.joinus.domain.tags.MemberTag;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Builder
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter @Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Member {
+
+    @EqualsAndHashCode.Include
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id", unique = true, nullable = false, updatable = false)
+    private Long id;
+
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String nickname;
+
+    private String phone;
+
+    private String profileImg;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    @Embedded
+    private Address address;
+
+    @Column(nullable = false)
+    private Double credibility = 45.0;
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.PERSIST, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<MemberTag> memberTag;
+
+    @OneToMany(mappedBy = "fromMember", fetch = FetchType.LAZY)
+    private List<Follow> followings = new ArrayList<>();
+
+    @OneToMany(mappedBy = "toMember", fetch = FetchType.LAZY)
+    private List<Follow> followers = new ArrayList<>();
+
+    @OneToMany(mappedBy = "author", fetch = FetchType.LAZY)
+    private List<Post> posts = new ArrayList<>();
+
+    @OneToMany(mappedBy = "member", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<PostLike> postLikes = new ArrayList<>();
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public void addMemberTag(MemberTag memberTag) {
+        if (memberTag != null) {
+            if (this.memberTag == null) {
+                this.memberTag = new ArrayList<>();
+            }
+            this.memberTag.add(memberTag);
+            memberTag.setMember(this); // 양방향 설정
+        }
+    }
+}
