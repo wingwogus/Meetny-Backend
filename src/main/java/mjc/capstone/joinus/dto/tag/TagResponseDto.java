7904d59a14177043563e5789c0bbecf1d2dfc773
monikhyun <mih2001103@naver.com>
2025-06-04 23:31:15 +0900

    feat : Tag Service 및 Controller 추가

---
+++ src/main/java/mjc/capstone/joinus/dto/tag/TagResponseDto.java
@@ -0,0 +1,27 @@
+package mjc.capstone.joinus.dto.tag;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Objects;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TagResponseDto {
+    private Long id;
+    private String tagName;
+    private String color;
+    private String category;
+
+    private boolean checked;
+
+    public TagResponseDto(Long id, String tagName, String color, Object category) {
+        this.id = id;
+        this.tagName = tagName;
+        this.color = color;
+        this.category = category.getClass().getSimpleName();
+        this.checked = false;
+    }
+}
