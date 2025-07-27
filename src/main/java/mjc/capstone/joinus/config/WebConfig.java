291266467d92c9d9498b17655339593b4533435c
이재현 <wingwogus@naver.com>
2025-07-27 11:04:32 +0900

    fix: 서버 Port 수정

--- src/main/java/mjc/capstone/joinus/config/WebConfig.java
+++ src/main/java/mjc/capstone/joinus/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("*")
                 .allowedMethods("*");
 
         registry.addMapping("/ws/**")
